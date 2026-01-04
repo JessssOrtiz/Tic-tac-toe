cf5171af805b6fe9b95be5fd2430eedeb153f8bc
JessssOrtiz <jessssortiz@gmail.com>
2026-01-04 16:04:54 -0600

    Se agrega función para mostrar el tablero.
    -Se añade función para mostrar el tablero.

--- tictactoecpp.cpp
+++ tictactoecpp.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 
+char tablero[3][3] = {
+{'1', '2', '3'},
+{'4', '5', '6'},
+{'7', '8', '9'}
+}
+
+char jugador = 'x'
+
+//Muestra el tablero
+void mostrarTablero() {
+    cout << "Tic tac toe\n\n";
+    for(int i=0; i < 3; i++) {
+        cout << " ";
+        for(int j = 0; j < 3; j++) {
+            cout << tablero[i][j];
+            if(j < 2) cout << " | ";
+        }
+        if(i < 2) cout << "\n---+---+---\n";
+    }
+    cout << "\n\n";
+}
